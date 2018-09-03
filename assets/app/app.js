90de5283c3e9e1eb4a255e4011c3d7d914a1e6d4
bryanlee <blee@utahsbr.edu>
2018-09-02 23:44:09 -0600

    added initial html and css for site structure, table for train data, and form for user input, started game functionality and form data actions as well as posting to firebase

--- assets/app/app.js
+++ assets/app/app.js
@@ -17,3 +17,64 @@
 //* Try adding `update` and `remove` buttons for each train. Let the user edit the row's elements-- allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
 
 //* As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. You'll need to read up on Firebase authentication for this bonus exercise.
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAOmMa5QrzqwbnEyp_ZQ31Aj3PSY7kQl4U",
+    authDomain: "train-scheduler-69b6a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-69b6a.firebaseio.com",
+    projectId: "train-scheduler-69b6a",
+    storageBucket: "train-scheduler-69b6a.appspot.com",
+    messagingSenderId: "534200773830"
+  }; firebase.initializeApp(config);
+
+// create easily accessible variable for firebase database
+var database = firebase.database();
+
+// GLOBAL VARIABLES
+
+// create an object that will hold the user's form input
+var trainInput = [];
+
+trainCount = 0;
+
+// function check database storage on page load
+function checkStorage() {
+
+    database.ref().on("value", function(snapshot) {
+        var trainData = JSON.stringify(snapshot.val());
+        console.log(snapshot.val().train1.info);
+
+    }); 
+
+} checkStorage();
+
+// when information is submitted via the scheduler form
+$("#schedule-submit").on("click", function(event) {
+    
+    //prevent default form submit action
+    event.preventDefault();
+
+    // push all user input to the trainInput array
+    trainInput.push($("#name-input").val());
+    trainInput.push($("#dest-input").val());
+    trainInput.push($("#time-input").val());
+    trainInput.push(parseInt($("#freq-input").val()));
+
+    // console.log the object to ensure it was appropriately filled in
+    console.log(trainInput);
+
+    // run function to fill in table data using user input
+    fillTable(trainInput);
+
+    // add the to train counter for data storage purposes
+    trainCount++
+
+    // update the database with the train information
+    database.ref("train" + trainCount).set({info: trainInput});
+})
+
+//function for filling in a new set of train data into the scheduler table
+function fillTable(userInput) {
+        $("tbody").append("<tr><th scope='row'>" + trainInput[0] + "</th><td>"+ trainInput[1] + "</td><td>" + trainInput[2] + "</td><td>" + trainInput[4] + "</td><td>" + "Stuff" + "</td></tr>");
+};
