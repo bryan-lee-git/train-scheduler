1636481515e7da0381a58a99f3821530f0f0ff12
bryanlee <blee@utahsbr.edu>
2018-09-04 21:28:38 -0600

    added in functions for getting current time, getting next train time based off of user current time, calculating minutes until next train arrival, fixed table sorting issues, still need to fix issue with table sort not refreshing once a new train is added via the database

--- assets/app/app.js
+++ assets/app/app.js
@@ -31,9 +31,11 @@ var config = {
     projectId: "train-scheduler-69b6a",
     storageBucket: "train-scheduler-69b6a.appspot.com",
     messagingSenderId: "534200773830"
-  }; firebase.initializeApp(config);
+  }; 
+  
+firebase.initializeApp(config);
 
-// create easily accessible variable for firebase database
+// create easily accessible, global variable for firebase database
 var database = firebase.database();
 
 // -----------------------------------------------------------------------------------------------------------------
@@ -41,58 +43,174 @@ var database = firebase.database();
 // -----------------------------------------------------------------------------------------------------------------
 
 // create empty array variables that will hold the user's form input and input saved on database
-var trainInput = [];
 var allTrains = [];
+var trainId = 0;
+var nextArrival = "";
+var minutesAway = "";
 
 // -----------------------------------------------------------------------------------------------------------------
-// FUNCTIONS
+// PROGRAM FUNCTIONS
 // -----------------------------------------------------------------------------------------------------------------
+
+//display user's local time in main jumbotron on page load
+var now = new Date().toLocaleTimeString();
+$("#local-time").text(now);
 
 // check database storage on page load
+// refer to the database
 database.ref().on("value", function(trainData) {
 
+    // set id to zero since we are loading the data into the table
+    trainId = 0;
+
+    // remove any info in the table body
     $("tbody").empty();
 
-    allTrains = trainData.val().allTrains;
+    // set the allTrains variable to equal the info located in the database
+    var loadedTrains = trainData.val().allTrains;
+    allTrains = loadedTrains;
 
+    // for every item currently in the database
     for (let i = 0; i < allTrains.length; i++) {
+        // grab each item
         var loadedTrain = allTrains[i];
+        // run it through the fillTable function to display in table
         fillTable(loadedTrain);
     }
 
-    $("#schedule-table").slideDown(2000);
+    // display the table on the page
+    $("#schedule-table").slideDown(400);
+
+    $(document).ready(function() {
+        $("#trains-table").DataTable();
+    });
+    
 }); 
 
 // when information is submitted via the scheduler form
-$("#schedule-form").on("click", "#schedule-submit", function(event) {
-    
+$("#schedule-submit").on("click", function(event) {
+        
     //prevent default form submit action
     event.preventDefault();
-
-    trainInput = [];
+    var trainInput = [];
+    trainId++;
 
     // push all user input to the trainInput array
     trainInput.push($("#name-input").val());
     trainInput.push($("#dest-input").val());
-    trainInput.push(parseInt($("#freq-input").val()));
+    trainInput.push($("#freq-input").val());
     trainInput.push($("#time-input").val());
+    trainInput.push(parseInt(trainId));
 
     allTrains.push(trainInput);
 
-    // run function to fill in table data using user input
-    fillTable(trainInput);
-
     // update the database with the train information
     database.ref().set({allTrains});
-})
+
+    // reset the form inputs
+    document.getElementById("schedule-form").reset();
+
+    // refresh displayed current time
+    $("#local-time").text(now);
+
+});
+
+// create all next train times based upon first train time
+function trainTimes(train) {
+
+    // time conversion functions
+    function timestrToSec(timestr) {
+        var parts = timestr.split(":");
+        return (parts[0] * 3600) + (parts[1] * 60) + (+parts[2]);
+    }
+    
+    function pad(num) {
+        if(num < 10) {
+            return "0" + num;
+        } else {
+            return "" + num;
+        }
+    }
+    
+    function formatTime(seconds) {
+        return [pad(Math.floor(seconds/3600)%60),
+            pad(Math.floor(seconds/60)%60),
+            pad(seconds%60),
+            ].join(":");
+    }
+
+    time1 = "00:00:" + train[2];
+    console.log(time1);
+    time2 = "00:" + train[3];
+    console.log(time2);
+
+    timeLeft = formatTime(timestrToSec("00:24:00") - timestrToSec(time2));
+    possibleRoutes = timestrToSec("00:24:00") / timestrToSec(time1);
+    arrivalTimes = ["00:00:00"];
+
+    function truncateString (str1, length) {
+  
+        if ( (str1.constructor === String) && (length > 0) ) {
+            return str1.slice(3, length);
+        }
+    };
+
+    for (let i = 0; i < possibleRoutes; i++) {
+
+        time2 = arrivalTimes[arrivalTimes.length-1];
+        nextTime = formatTime(timestrToSec(time1) + timestrToSec(time2));
+        nextArrival = truncateString(nextTime, 8);
+        arrivalTimes.push(nextTime);
+    }
+
+    date = "00:" + Date().slice(16,21);
+    routesLeft = Math.ceil(timestrToSec(date) / timestrToSec(time1));
+    console.log(routesLeft);
+    nextArrival = arrivalTimes[routesLeft];
+    minutesAway = timestrToSec(nextArrival) - timestrToSec(date);
+    nextArrival = truncateString(nextArrival, 8); 
+    console.log(minutesAway);
+}
+
+// calculate time until next train based upon next train time and frequency (in minutes)
 
 //function for filling in a new set of train data into the scheduler table
 function fillTable(userInput) {
+    trainTimes(userInput);
+    trainId++;
     $("tbody").append(
-        "<tr><th scope='row'>" + userInput[0] + "</th>"
+        "<tr id='" + trainId + "'>" 
+        + "<td>" + userInput[0] + "</td>"
         + "<td>" + userInput[1] + "</td>"
         + "<td>" + userInput[2] + "</td>"
-        + "<td>" + userInput[3] + "</td>"
-        + "<td>Stuff</td></tr>"
+        + "<td>" + nextArrival + "</td>"
+        + "<td>" + minutesAway + " min</td>" 
+        + "<td style='text-align:center;'><span value='" + trainId + "' class='trash-can'>🗑</span></td>"
+        + "</tr>"
     );
 };
+
+// delete row and stored data when trash can is clicked
+$("#trains-table").on("click", ".trash-can", function(event) {
+
+    //get unique identifier value to delete desired row of data from page and from storage
+    var deleteBtn = $(this).attr("value");
+
+    // prevent any default click functions
+    event.preventDefault();
+
+    // make sure we are getting the value we want
+    console.log(deleteBtn);
+
+    // remove the train from our storage array matchin the unique identfier
+    allTrains.splice([deleteBtn] - 1, 1);
+
+    // make sure we deleted what we wanted to
+    console.log(allTrains);
+
+    // update the database with the new data
+    database.ref().set({allTrains});
+
+    $(deleteBtn.parent).remove();
+
+})
